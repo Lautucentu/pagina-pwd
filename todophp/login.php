cb91ca4e68481e89eb4402bec274d22cbe755c54
Lautucentu <centurionlautaro2@gmail.com>
2023-11-08 20:43:57 -0300

    a ver

--- todophp/login.php
+++ todophp/login.php
@@ -30,14 +30,14 @@ if ($result->num_rows == 1) {
         echo "
         <script>
           alert('Usuario inexistente');
-          window.location = 'login.php';
+          window.location = '../login.php';
         </script>";
     }
 } else {
     echo "
     <script>
       alert('Usuario no registrado');
-      window.location = 'register.php';
+      window.location = '../register.php';
     </script>";}
 
 // Cerrar la conexión
