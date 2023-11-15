3d63fba30b3d91b5da1dca60b2961fe32d44fd07
Lautucentu <centurionlautaro2@gmail.com>
2023-11-15 18:41:58 -0300

    123

--- cartelera.php
+++ cartelera.php
@@ -1,3 +1,21 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['usuario'])){
+    echo "
+        <script>
+            alert('Por favor debes iniciar sesion');
+            window.location = 'index.php';
+        </script>
+    ";
+    header("location: index.php");
+    session_destroy();
+    die();
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
